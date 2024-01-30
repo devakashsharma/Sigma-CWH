3b59330f2e37194e6471268185e64a3e37404ecc
devakashsharma <soulakash20002@gmail.com>
2024-01-30 17:48:46 +0530

    Error Handling

---
+++ Day 78 - Exercise 14/script.js
@@ -0,0 +1,9 @@
+async function hecking() {
+    
+}
+
+async function main () {
+    console.log("Initializing Hacking...");
+    console.log("Initializing reading files...")
+    console.log("Detecting password")
+}
